3a94a3a6ad9aa21d230dd626b05e01c51bd421e2
Erick Lumunge <lumungep12@gmail.com>
2021-10-22 18:32:11 +0300

    footer headings

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "97eed9a9475dc6be95ff"; }
+/******/ 		__webpack_require__.h = function() { return "54e7e5ae207aaa6dec07"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
